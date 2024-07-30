76c021f16d37fb3f82d4917fed30ec89062ecdc1
LeonidAbrahamyan17 <leonid17abrahamyan@gmail.com>
2024-07-30 11:28:00 -0700

    Initial commit

---
+++ src/storage/ProductStorage.java
@@ -0,0 +1,52 @@
+package storage;
+
+import model.Product;
+import util.StorageSerializeUtil;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ProductStorage implements Serializable {
+
+    private Set<Product> products = new HashSet<>();
+
+    public void add(Product product) {
+        products.add(product);
+        StorageSerializeUtil.serializeProductStorage(this);
+    }
+
+    public Product getById(String id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void print() {
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
+    public void delete(String productId) {
+//        for (Product product : products) {
+//            if (product.getId().equals(productId)) {
+//                products.remove(product);
+////                break;
+//            }
+//        }
+
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product next = iterator.next();
+            if (next.getId().equals(productId)) {
+                iterator.remove();
+            }
+        }
+    }
+
+}
